039782c98801da247dd6406dc58677eed8226f24
Corvaxx117 <julien.amiel117@gmail.com>
2025-05-08 00:44:10 +0200

    Renommage en metroid-webapp

--- bin/post-install.php
+++ bin/post-install.php
@@ -8,15 +8,18 @@ $reset = "\033[0m";
 
 // ASCII art stylisé du mot "M  i  n  i"
 $logo = <<<ASCII
-{$cyan}
- __  __            _                        _            
-|  \/  |          | |    _____      ___    (_)   ___        
-| \  / |   ____  _| |_  |   _ \   /  _  \   _   |    \   
-| |\/| |  |  __| \  __| |  (_)|   | | | |  | |  |  |  |
-| |  | |  |  __|  | |_  |  |\  \  | |_| |  | |  |  |  |
+{$cyan}  
+
+༼ つ ಥ_ಥ ༽つ    ༼ つ ಥ_ಥ ༽つ    ༼ つ ಥ_ಥ ༽つ    ༼ つ ಥ_ಥ ༽つ
+                   _                        _
+ __  __           | |                      (_)            
+|  \/  |   ____  _| |_   _____      ___     _    ___        
+| \  / |  |  __| \   _| |   _  \  /  _  \  | |  |    \   
+| |\/| |  |  \    | |   |  (_) |  | | | |  | |  |  |  |
+| |  | |  |  /_   | |_  |  |\  \  | |_| |  | |  |  |  |
 |_|  |_|  |____|  \___| |__| \__| \_____/  |_|  |____/
 
-  ______
+  
 ༼ つ ಥ_ಥ ༽つ    ༼ つ ಥ_ಥ ༽つ    ༼ つ ಥ_ಥ ༽つ    ༼ つ ಥ_ಥ ༽つ
 
       {$orange}M   i   n   i{$reset}
@@ -28,5 +31,5 @@ echo $logo . "\n";
 echo "\n{$green}✅ Installation terminée avec succès !{$reset}\n";
 echo "🚀 Pour lancer le serveur local : {$cyan}php -S localhost:8000 -t public{$reset}\n";
 echo "📂 Structure de base créée : src/, config/, public/, views/\n";
-echo "🧠 Pour plus d'infos, consultez la documentation dans le README.md\n";
-echo "📚 Documentation : https://github.com/Corvaxx117/metroid-webapp\n";
+echo "📂 Framework Metroid installé \n";
+echo "🧠 Pour plus d'infos, consultez la documentation dans le README.md ou https://github.com/Corvaxx117/metroid-webapp\n";
